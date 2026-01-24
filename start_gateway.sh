2ee1888540a555ac8379733ee7a3dac4dff3c2c7
AbouTame <abou.tame99@gmail.com>
2026-01-24 03:46:34 +0100

    fixes

--- start_gateway.sh
+++ start_gateway.sh
@@ -16,7 +16,6 @@ fi
 # Install system dependencies for Puppeteer (Chrome)
 echo "Installing system dependencies..."
 if [ -f /etc/debian_version ]; then
-    apt-get update
     apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2 libpango-1.0-0 libpangocairo-1.0-0
 fi
 
